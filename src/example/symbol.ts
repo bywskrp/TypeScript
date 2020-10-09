81c5a45fcdace4449bf17d16333644f1089eaa2d
wxh <wherehaveun@gmail.com>
2020-10-09 18:58:00 +0800

    symbol.ts

--- src/example/symbol.ts
+++ src/example/symbol.ts
@@ -1,10 +1,53 @@
-const s1 = Symbol()
-const s2 = Symbol()
+// const s1 = Symbol()
+// const s2 = Symbol()
 
-// console.log(s1 === s2) 
+// // console.log(s1 === s2) 
 
-const s3 = Symbol("wxh")
-const s4 = Symbol("wxh")
+// const s3 = Symbol("wxh")
+// const s4 = Symbol("wxh")
 
-// console.log(s3 === s4) 
+// // console.log(s3 === s4) 
 
+// console.log(s3)
+
+// let prop = "name"
+// const info = {
+//   // name: "wxh"
+//   [prop]: "wxh"
+// }
+// console.log(info)
+
+// let s5 = Symbol("name")
+// const info2 = {
+//   [s5]: "wxh",
+//   age: 18,
+//   sex: "man"
+// }
+
+// console.log(info2)
+
+// // info2[s5] = "sly"
+
+// for (const key in info2) {
+//   console.log(key);
+// }
+
+// console.log(Object.keys(info2))
+// console.log(Object.getOwnPropertyNames(info2))
+
+interface GetProp {
+  <T, U extends keyof T>(object: T, prop: U)
+}
+
+const getProp: GetProp = (object, prop) => {
+  return object[prop]
+}
+
+const obj = {
+  a: 'a',
+  b: 'b'
+}
+
+console.log(getProp(obj, 'a'))
+console.log(getProp(obj, 'b'))
+console.log(getProp(obj, 'c'))
